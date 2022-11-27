023899467d61daa716cf2c349463f9febda92467
cym <pharmadev@proton.me>
2022-11-27 13:10:03 +0100

    Add main folder

---
+++ FlashCardsApp/views.py
@@ -0,0 +1,10 @@
+from random import shuffle
+from .controller import MainApp
+from .models import create_dictionary
+
+def app():
+    dct = create_dictionary()
+    keyslist = list(dct.keys())
+    shuffle(keyslist)
+    app = MainApp(keyslist,dct)
+    app.mainloop()
