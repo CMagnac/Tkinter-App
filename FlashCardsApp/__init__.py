c66f030d3cfb59b68b32adb74e30712c8c5ec591
cym <pharmadev@proton.me>
2022-11-28 19:19:50 +0100

    Upgrade code readability

--- FlashCardsApp/__init__.py
+++ FlashCardsApp/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['controller','models','views']
+__all__ = ['controller', 'models', 'views']
--- FlashCardsApp/views.py
+++ FlashCardsApp/views.py
@@ -2,9 +2,10 @@ from random import shuffle
 from .controller import MainApp
 from .models import create_dictionary
 
+
 def app():
     dct = create_dictionary()
     keyslist = list(dct.keys())
     shuffle(keyslist)
-    app = MainApp(keyslist,dct)
+    app = MainApp(keyslist, dct)
     app.mainloop()
